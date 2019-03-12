c1e5583c9fd6b43e0cd3d70a56337408485a3178
bxxstian <seb_faull@hotmail.co.uk>
2019-03-12 11:56:32 +0000

    Open and create file system

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] data = {
+                "Line 1",
+                "Line 2 2",
+                "Line 3 3 3",
+                "Line 4 4 4 4",
+                "Line 5 5 5 5 5"
+        };
+
+        try (FileSystem zipFs = openZip(Paths.get("src/main/java/myData.zip"))) {
+
+        } catch (Exception e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
+    }
+
+    private static FileSystem openZip(Path zipPath) throws IOException, URISyntaxException {
+        Map<String, String> providerProps = new HashMap<>();
+        providerProps.put("create", "true");
+
+        URI zipUri = new URI("jar:file", zipPath.toUri().getPath(), null);
+        FileSystem zipFs = FileSystems.newFileSystem(zipUri, providerProps);
+
+        return zipFs;
+    }
+
+    private static void copyToZip(FileSystem zipFs) throws IOException {
+
+    }
+
+    private static void writeToFileInZip1(FileSystem zipFs, String[] data) throws IOException {
+
+    }
+}
